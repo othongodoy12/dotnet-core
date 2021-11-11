9041fb8c22c056be5e4dbb3c1855eb939ed3c541
Othon Godoy <othon.dev@outlook.com>
2021-11-11 20:52:59 -0300

    first commit

---
+++ hangfire/HangfireApp/Services/JobTestService.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace HangfireApp
+{
+    public interface IJobTestService
+    {
+        void FireAndForgetJob();
+        void ReccuringJob();
+        void DelayedJob();
+        void ContinuationJob();
+    }
+
+    public class JobTestService : IJobTestService
+    {
+        public void FireAndForgetJob()
+        {
+            Console.WriteLine("Hello from a Fire and Forget job!");
+        }
+        
+        public void ReccuringJob()
+        {
+            Console.WriteLine("Hello from a Scheduled job!");
+        }
+
+        public void DelayedJob()
+        {
+            Console.WriteLine("Hello from a Delayed job!");
+        }
+        
+        public void ContinuationJob()
+        {
+            Console.WriteLine("Hello from a Continuation job!");
+        }
+    }
+}
